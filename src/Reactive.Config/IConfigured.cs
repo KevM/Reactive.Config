3e51f3489d4bd1de8bf68e3ada1d8fe28b1f28d3
Kevin Miller <scoundrel@gmail.com>
2016-11-21 15:23:06 -0600

    remove unused example

--- src/Reactive.Config/IConfigured.cs
+++ src/Reactive.Config/IConfigured.cs
@@ -1,12 +1,4 @@
-using System;
-
 namespace Reactive.Config
 {
     public interface IConfigured {}
-
-    public class MyExampleConfiguration : IConfigured {
-        public bool IsEnabled { get;set;} = false;
-        public DateTime EnabledOn { get;set;} = new DateTime(2017, 1, 1, 10, 30, 0);
-        public string ShutoffMessage { get;set;} = "Sorry this feature has been disabled";
-    }
 }
