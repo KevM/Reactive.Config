ae2a7fc51ac9bdc2ce9e0466b304de34287bad7b
Kevin Miller <scoundrel@gmail.com>
2016-11-22 16:43:53 -0600

    Start of environment source

---
+++ src/Reactive.Config/Sources/EnvironmentConfigurationSource.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reflection;
+
+namespace Reactive.Config.Sources
+{
+    public class EnvironmentConfigurationSource : IConfigurationSource
+    {
+        private readonly IKeyPathProvider _keyPathProvider;
+
+        public EnvironmentConfigurationSource(IKeyPathProvider keyPathProvider)
+        {
+            _keyPathProvider = keyPathProvider;
+        }
+
+        public bool Handles<T>() where T : class, IConfigured, new()
+        {
+            return ConfiguredEnvironmentEntries<T>().Any();
+        }
+
+        public ConfigurationResult<T> Get<T>(ConfigurationResult<T> result) where T : class, IConfigured, new()
+        {
+            var configured = new T();
+//            var propertiesByName = typeof(T).GetProperties(BindingFlags.Instance | BindingFlags.Public).ToDictionary(k=>k.Name);
+//            var propertiesFound = GetConfiguredDictionary<T>()
+//
+//            foreach (var property in propertiesFound)
+//            {
+//
+//            }
+
+            return new ConfigurationResult<T>(configured, Observable.Never<T>());
+        }
+
+        private IDictionary<string, string> GetConfiguredDictionary<T>() where T : class, IConfigured, new()
+        {
+            return ConfiguredEnvironmentEntries<T>()
+                .ToDictionary(k => GetKeyTail((string) k.Key), v => (string) v.Value);
+        }
+
+        private IEnumerable<DictionaryEntry> ConfiguredEnvironmentEntries<T>() where T : class, IConfigured, new()
+        {
+            var keyPrefix = _keyPathProvider.GetKeyPath<T>();
+
+            return Environment.GetEnvironmentVariables()
+                .OfType<DictionaryEntry>()
+                .Where(entry => ((string) entry.Key).StartsWith(keyPrefix));
+        }
+
+        public static string GetKeyTail(string key)
+        {
+            var indexOfLastDot = key.LastIndexOf(".", StringComparison.InvariantCulture);
+
+            return indexOfLastDot < 0 ? key : key.Substring(indexOfLastDot+1);
+        }
+    }
+}
---
+++ src/Reactive.Config/Sources/FastYetSimpleTypeActivator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace Reactive.Config.Sources
+{
+    public class FastYetSimpleTypeActivator
+    {
+        private static readonly Dictionary<Type, Func<object>> Ctors = new Dictionary<Type, Func<object>>();
+
+        public static object CreateInstance(Type type)
+        {
+            if (Ctors.ContainsKey(type)) return Ctors[type]();
+
+            var exp = Expression.New(type);
+            var d = Expression.Lambda<Func<object>>(exp).Compile();
+
+            if (!Ctors.ContainsKey(type))
+            {
+                Ctors.Add(type, d);
+            }
+
+            return Ctors[type]();
+        }
+    }
+}
